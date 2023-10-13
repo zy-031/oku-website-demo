f38b0e3ebcda1e8ccc17e226f490bf9d83a04ff6
zy-031 <zhenyao@1utar.my>
2023-10-13 23:23:33 +0800

    boilerpltae for website

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -5,5 +5,21 @@ const tokens = (n) => {
 }
 
 describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
+
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners()
+
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+  })
+
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+  })
+
 
 })
